6dd705152e2e041e41a24ce8e070a15690774f7f
leopard1218 <snowleopards1218@gmail.com>
2022-06-13 13:21:36 +0300

    size

--- src/components/Card.js
+++ src/components/Card.js
@@ -26,7 +26,7 @@ const Card = ({ data, setCards }) => {
         <div>
             <div className="ui card fluid">
                 <div className="image">
-                    <img src={data.image} />
+                    <img height="200px" width="100px" src={data.image} />
                 </div>
                 <div className="content">
                     <div className="header">{data.title}</div>
